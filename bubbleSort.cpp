ad8989937f2b78673e54be7df038aa7dbef6882c
jason <jbierbrauer99@gmail.com>
2017-05-29 11:23:05 -0500

    second draft of bubble sort c++

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -21,9 +21,32 @@ int main()
 			}
 		}
 	}
+	
+	/* Second way of sorting using ternary operator */
 
+	int nums2[] = {44, 77, 65, 23, 65, 37, 97, 26, 91, 96, 45, 68, 39};
+	int size2 = distance(begin(nums2), end(nums2));
+	for (int i = 0; i < size2; i++)
+	{
+		for (int j = 0; j < size2 -1; j++)
+		{
+			int temp;
+			temp = (nums2[j] > nums2[j+1]) ?
+			 (temp = nums2[j]) & (nums2[j] = nums2[j+1]) & (nums2[j+1] = temp) :
+			   nums2[j];
+		}
+	}
+	
+	// printing loops
 	for (int n = 0; n < size; n++)
 	{
 		printf("%d\n", nums[n]);
+	}
+	// newling between outputs
+	printf("\n");
+
+	for (int n = 0; n < size2; n++)
+	{
+		printf("%d\n", nums2[n]);
 	}
 }
