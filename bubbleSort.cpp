f0cbf2fb1635492becfb6188f7522b97552bb403
jason <jbierbrauer99@gmail.com>
2017-05-30 10:43:20 -0500

    encapsulated into function

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,6 +1,38 @@
 #include <iostream>
 using namespace std;
 
+// just use void function and pass size instead of
+// trying to return an array
+void sort(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size-1; j++)
+		{
+			if (arr[j] > arr[j+1])
+			{
+				int temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+}
+
+void bubbleSort(int arr[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = 0; j < size-1; j++)
+		{
+			int temp = (arr[j] > arr[j+1]) ?
+					(temp = arr[j]) &
+					(arr[j] = arr[j+1]) &
+					(arr[j+1] = temp) : arr[j];
+		}
+	}
+}
+
 int main()
 {
 	/* Need to read more about passing arrays to functions 
@@ -42,11 +74,30 @@ int main()
 	{
 		printf("%d\n", nums[n]);
 	}
-	// newling between outputs
+	// newline between outputs
 	printf("\n");
 
 	for (int n = 0; n < size2; n++)
 	{
 		printf("%d\n", nums2[n]);
+	}
+	// new line between outputs
+	printf("\n");
+	
+	int size3 = distance(begin(nums2), end(nums2));
+	sort(nums2, size3);
+	for (int n = 0; n < size3; n++)
+	{
+		printf("%d\n", nums2[n]);
+	}
+	// new line between outputs
+	printf("\n");
+
+	int nums3[] = {44, 77, 65, 23, 65, 37, 97, 26, 91, 96, 45, 68, 39};
+	int size4 = distance(begin(nums3), end(nums3));
+	bubbleSort(nums3, size4);
+	for (int n = 0; n < size4; n++)
+	{
+		printf("%d\n", nums3[n]);
 	}
 }
